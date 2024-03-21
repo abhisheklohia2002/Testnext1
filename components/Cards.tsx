5187e17c429b39b853771df2935207716b193a6c
abhishek lohia <abhisheklohiamarketplace@gmail.com>
2024-03-21 11:27:45 +0530

    added

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -11,22 +11,20 @@ export default function Cards(props: any) {
         key={props.data.id}
         className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
       >
-        <a href="" className="">
-          <img
-            className="rounded-t-lg h-48 relative left-28"
-            src={props.data.image}
-            alt={props.data.title}
-          />
-        </a>
+        <img
+          className="rounded-t-lg h-48 relative left-28"
+          src={props.data.image}
+          alt={props.data.title}
+        />
+
         <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {props.data.title}
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {props.data.title}
+          </h1>
+
+          <h1 className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {props.data.description}
-          </p>
+          </h1>
           <Link
             href={`product/${props.data.id}`}
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,9 +14,9 @@ export default function NavbarComponent() {
               className="h-8"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <h1 className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               TripXl
-            </span>
+            </h1>
           </a>
           <button
             data-collapse-toggle="navbar-multi-level"
@@ -47,7 +47,7 @@ export default function NavbarComponent() {
             id="navbar-multi-level"
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
+              <h1>
                 <a
                   href="#"
                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
@@ -55,8 +55,8 @@ export default function NavbarComponent() {
                 >
                   Home
                 </a>
-              </li>
-              <li>
+              </h1>
+              <h1>
                 <button
                   id="dropdownNavbarLink"
                   data-dropdown-toggle="dropdownNavbar"
@@ -88,14 +88,14 @@ export default function NavbarComponent() {
                     className="py-2 text-sm text-gray-700 dark:text-gray-200"
                     aria-labelledby="dropdownLargeButton"
                   >
-                    <li>
+                    <h1>
                       <a
                         href="#"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         Dashboard
                       </a>
-                    </li>
+                    </h1>
                     <li aria-labelledby="dropdownNavbarLink">
                       <button
                         id="doubleDropdownButton"
@@ -129,49 +129,49 @@ export default function NavbarComponent() {
                           className="py-2 text-sm text-gray-700 dark:text-gray-200"
                           aria-labelledby="doubleDropdownButton"
                         >
-                          <li>
+                          <h1>
                             <a
                               href="#"
                               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                             >
                               Overview
                             </a>
-                          </li>
-                          <li>
+                          </h1>
+                          <h1>
                             <a
                               href="#"
                               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                             >
                               My downloads
                             </a>
-                          </li>
-                          <li>
+                          </h1>
+                          <h1>
                             <a
                               href="#"
                               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                             >
                               Billing
                             </a>
-                          </li>
-                          <li>
+                          </h1>
+                          <h1>
                             <a
                               href="#"
                               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                             >
                               Rewards
                             </a>
-                          </li>
+                          </h1>
                         </ul>
                       </div>
                     </li>
-                    <li>
+                    <h1>
                       <a
                         href="#"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         Earnings
                       </a>
-                    </li>
+                    </h1>
                   </ul>
                   <div className="py-1">
                     <a
@@ -182,31 +182,31 @@ export default function NavbarComponent() {
                     </a>
                   </div>
                 </div>
-              </li>
-              <li>
+              </h1>
+              <h1>
                 <a
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Services
                 </a>
-              </li>
-              <li>
+              </h1>
+              <h1>
                 <a
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Pricing
                 </a>
-              </li>
-              <li>
+              </h1>
+              <h1>
                 <a
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Contact
                 </a>
-              </li>
+              </h1>
             </ul>
           </div>
         </div>
